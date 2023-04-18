459cc97b4e0fdcffbd1b1b95b6dd9e2c57dd0a7d
mrilia <mrilia@DEV-PC>
2023-04-18 15:47:46 +0430

    test: add persistence layer

---
+++ TicketingSolution.Persistence/Repositories/TicketBookingService.cs
@@ -0,0 +1,28 @@
+﻿using TicketingSolution.Core.DataServices;
+using TicketingSolution.Domain;
+
+namespace TicketingSolution.Persistence.Repositories
+{
+    public class TicketBookingService : ITicketBookingService
+    {
+        private readonly TicketingSolutionDbContext _context;
+
+        public TicketBookingService(TicketingSolutionDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IEnumerable<Ticket> GetAvailabeTickets(DateTime Date)
+        {
+            return _context.Tickets
+                .Where(q => !q.TicketBooking.Any(x => x.Date == Date))
+                .ToList();
+        }
+
+        public void Save(TicketBooking ticketBooking)
+        {
+            _context.Add(ticketBooking);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ TicketingSolution.Persistence/TicketingSolutionDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSolution.Domain;
+
+namespace TicketingSolution.Persistence
+{
+    public class TicketingSolutionDbContext : DbContext
+    {
+      
+
+        public TicketingSolutionDbContext(DbContextOptions<TicketingSolutionDbContext> options) : 
+            base(options)
+        {
+
+        }
+
+
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketBooking> TicketBookings { get; set; }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Ticket>().HasData(
+                new Ticket { Id = 1, Name = "To Shiraz" },
+                new Ticket { Id = 2, Name = "To Esfahan" },
+                new Ticket { Id = 3, Name = "To Mashhad" }
+            );
+        }
+
+
+    }
+}
