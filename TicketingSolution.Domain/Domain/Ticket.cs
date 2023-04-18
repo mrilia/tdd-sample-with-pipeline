d8f8b6f188f788343254d1e8b931e88a1e3b6389
mrilia <mrilia@DEV-PC>
2023-04-18 23:13:12 +0430

    refactor: a bit refactor

--- TicketingSolution.Domain/Domain/Ticket.cs
+++ TicketingSolution.Domain/Domain/Ticket.cs
@@ -1,8 +1,7 @@
 ﻿namespace TicketingSolution.Domain
 {
     public class Ticket
-    {
-        
+    {        
         public int Id { get; set; }
         public string Name { get; set; }
         public List<TicketBooking> TicketBooking { get; set; }
